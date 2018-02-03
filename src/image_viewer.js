3c980ff57b60e19241c9e34fd1c7732692a2e51f
Vincent Yang <mscsharp@gmail.com>
2018-02-04 00:12:52 +1100

    Add css loader, style loader and ExtractTextWebpackPlugin to include CSS build into webpack

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,11 @@
+import '../styles/image_viewer.css';
+
 const image = document.createElement('img');
 
-image.src = 'http://via.placeholder.com/400x400';
+//image.src = 'http://via.placeholder.com/400x400';
+
+//image.src = 'https://picsum.photos/400/400/?random';
+image.src = 'http://placeimg.com/400/400/any';
 
 document.body.appendChild(image);
 
