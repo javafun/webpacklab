1ff1bff008ccde06a13381e70845fee61981b2c7
Vincent Yang <mscsharp@gmail.com>
2018-02-06 21:32:50 +1100

    Completed code splitting demo

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,14 +1,8 @@
 import '../styles/image_viewer.css';
-import big from '../assets/large.jpg';
 import small from '../assets/small.jpg';
 
-
+export default ()=>{
 const image = document.createElement('img');
 image.src = small;
 document.body.appendChild(image);
-
-
-const bigImage = document.createElement('img');
-bigImage.src = big;
-document.body.appendChild(bigImage);
-
+};
